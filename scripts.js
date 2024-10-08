1e33537b03036840652165593c2baec75d7132ba
Késia Vitória <k.gomeskesia@gmail.com>
2024-10-08 15:13:14 -0300

    Formatando a moeda

--- scripts.js
+++ scripts.js
@@ -9,6 +9,20 @@ amount.oninput = () => {
     // Obtém o valor atual do input e remove os caracteres não numéricos.
     let value = amount.value.replace(/\D/g, "")
 
+    // Transforma o valor em centavos (ex.: 120/100 = 1.2 que é equivalente a R$1,20).
+    value = Number(value) / 100
+
     // Atualiza o valor do input.
-    amount.value = value
+    amount.value = formatCurrencyBRL(value)
+}
+
+function formatCurrencyBRL(value){
+
+    // Formata o valor no padrão BRL (Real Brasileiro).
+    value = value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    })
+
+    return value
 }
