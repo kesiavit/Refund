6ed47e6410694fedb1dca36e0c2efc9da46e9fc2
Késia Vitória <k.gomeskesia@gmail.com>
2024-10-09 20:52:04 -0300

    Cuidando da usabilidade

--- scripts.js
+++ scripts.js
@@ -99,6 +99,9 @@ function expenseAdd(newExpense) {
         // Adiciona o item na lista.
         expenseList.append(expenseItem)
 
+        // Limpa o formulário para adicionar um novo item.
+        formClear()
+
         // Atualiza os totais.
         updateTotals()
 
@@ -175,3 +178,12 @@ expenseList.addEventListener("click", function(event) {
     // Atualiza os totais
     updateTotals()
 })
+
+function formClear(){
+    expense.value = ""
+    category.value = ""
+    amount.value = ""
+
+    // Coloca o foco no input de amount.
+    expense.focus()
+}
